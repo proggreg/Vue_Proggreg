42f66adbb8fd6ba358fd54f3f702c44494d546c9
Greg Field <greg.field1992@gmail.com>
2020-09-15 16:23:07 +0100

    update vue config file for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      "": {
+        target: "http://localhost:4000"
+      }
+    }
+  },
+  outputDir: path.resolve(__dirname, '../proggreg-backend/public')
 };
