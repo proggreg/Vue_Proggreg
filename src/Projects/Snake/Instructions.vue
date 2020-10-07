a0e55ab9fce5aeae91d8614e6cb5dfe9f0f31d3b
proggreg <greg.field1992@gmail.com>
2020-10-07 16:10:34 +0100

    changed to detect touch screen

--- src/Projects/Snake/Instructions.vue
+++ src/Projects/Snake/Instructions.vue
@@ -1,37 +1,42 @@
 <template>
-  <v-container style="height: 100%;" class="full-width">
-    <v-layout id="instructionsCard" column style="height: 80%;" justify-center align-center>
-      <!-- Mobile -->
-      <v-container v-if="isMobile">
-        <v-card-text
-          style="font-size: 1.2rem;"
-          class="primary--text pa-2 text-center"
-        >Swipe the screen in the direction you want to make the snake move.</v-card-text>
-        <v-img src="@/assets/swipe.png"></v-img>
+  <v-container column class="text-center">
+    <!-- Mobile -->
+    <div v-if="isTouchEnabled">
+      <v-card-text class="primary--text" style="font-size: 100%"
+        >Swipe the screen to make the snake move.</v-card-text
+      >
+      <v-container style="display: flex; width: 100px">
+        <v-img contain src="@/assets/swipe.png"></v-img>
       </v-container>
+    </div>
 
-      <!-- Desktop -->
-      <v-container v-else>
-        <v-card-text
-          style="font-size: 1.2rem; text-align: center; margin-bottom: 20px;"
-          class="primary--text text--center"
-        >Use arrow keys to move the snake.</v-card-text>
-        <v-layout justify-center align-center>
-          <v-img max-width="50%" src="@/assets/arrowkeys.png"></v-img>
-        </v-layout>
+    <!-- Desktop -->
+    <div v-else>
+      <v-card-text class="primary--text" style="font-size: 100%">
+        Press arrow keys to move the snake.
+      </v-card-text>
+
+      <v-container style="width: 40%" justify-center align-center>
+        <v-img contain src="@/assets/arrowkeys.png"></v-img>
       </v-container>
-    </v-layout>
+    </div>
   </v-container>
 </template>
 
 <script>
-/* eslint-disable */
 export default {
   name: "Instructions",
-  data() {
-    return {
-      isMobile: mobileCheck(),
-    };
+  created() {
+    console.log(this.is_touch_enabled());
+  },
+  methods: {
+    isTouchEnabled() {
+      return (
+        "ontouchstart" in window ||
+        navigator.maxTouchPoints > 0 ||
+        navigator.msMaxTouchPoints > 0
+      );
+    },
   },
 };
 </script>
