7b432d66e69914e647a59ce73e45262687424991
proggreg <greg.field1992@gmail.com>
2020-10-13 11:54:33 +0100

    improve layout of tab buttons

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,24 +1,22 @@
 <template>
-  <div style="width: fit-content" class="pa-0 ma-0">
-    <BaseButton
-      style="padding: 1em 4em;"
-      class="ma-2"
-      :to="tab.component"
-      :key="tab.title"
+  <v-card style="display: flex">
+    <v-tabs
       v-for="tab in tabs"
-    >{{tab.title}}</BaseButton>
-  </div>
+      :key="tab.title"
+      background-color="topbar"
+      icons-and-text
+      grow
+    >
+      <v-tab :href="tab.component">
+        {{ tab.title }}
+        <v-icon>{{ tab.icon }}</v-icon>
+      </v-tab>
+    </v-tabs>
+  </v-card>
 </template>
-<style scoped>
-</style>
 <script>
-import BaseButton from "./BaseButton";
-/* eslint-disable */
 export default {
   name: "Tabs",
-  components: {
-    BaseButton,
-  },
   data() {
     return {
       tabs: [
@@ -38,15 +36,13 @@ export default {
           component: "/Contact",
         },
       ],
-      isMobile: mobileCheck,
     };
   },
 };
 </script>
-<style lang="scss">
-.tab {
-  @media screen and (max-width: 600px) {
-    width: 10px !important;
-  }
+
+<style >
+.v-tabs-slider-wrapper {
+  display: none;
 }
 </style>
